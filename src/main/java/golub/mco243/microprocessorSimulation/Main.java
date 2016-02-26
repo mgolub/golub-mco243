c7d3a5eeb929e57d3cac0e177fb16b1292944a30
Malkie Golub <Malkie Golub>
2016-02-25 21:54:04 -0500

    added mach.in and switched to console application

--- src/main/java/golub/mco243/microprocessorSimulation/Main.java
+++ src/main/java/golub/mco243/microprocessorSimulation/Main.java
@@ -4,13 +4,15 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 
-		BufferedReader input = new BufferedReader(new FileReader("mach.in"));
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		//BufferedReader input = new BufferedReader(new FileReader("mach.in"));
 		String line;
 		while ((line = input.readLine()) != null) {
 			char[] inputInstruction = line.toCharArray();
